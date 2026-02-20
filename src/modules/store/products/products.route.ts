62599da095d108b38e884c7d2f3dcce251d23e67
Thoriq Rahman <thoriqrh60s@gmail.com>
2026-02-21 04:35:53 +0700

    little fix route order

--- src/modules/store/products/products.route.ts
+++ src/modules/store/products/products.route.ts
@@ -9,10 +9,10 @@ const repo = new ProductRepo();
 const service = new ProductService(repo);
 const controller = new ProductController(service);
 
-router.get("/:slug", controller.getProductDetail);
-router.get("/:productSlug/variants/:variantId", controller.getVariantDetail);
 router.get("/by-category", controller.getByCategory);
 router.get("/by-collection", controller.getByCollection);
 router.get("/by-search", controller.getBySearch);
+router.get("/:slug", controller.getProductDetail);
+router.get("/:productSlug/variants/:variantId", controller.getVariantDetail);
 
 export default router;
